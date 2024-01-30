6a7ba546549c0a534276174aa39878196aacc583
apersis <apersis@laposte.net>
2024-01-30 16:34:26 +0100

    Update javafx

---
+++ Documentation API.md
@@ -0,0 +1,21 @@
+# DOCUMENTATION API
+
+## Informations
+
+Pour notre projet scolaire intitulé "Développer une application informatique" il nous à été demandé de créer une API REST. Hormis pour la l'operation **DELETE**, où la réponse est un booléen sur la réussite ou non de la suppression, les reponses sont sous le format JSON
+
+## Accès
+
+Notre API n'est pas privé, nulle besoin de s'autentifier pour y accèder.
+
+## Arborèscence
+
+Vous trouverez ci-dessous la liste des tables disponible de notre base de donnée. Elles sont toutes un accès CRUD : C'est à dire que vous pourrez :
+
+Créer une nouvelle instance (Create)
+
+Reccuperer toutes les instances ou uniquement celle de votre choix (Read)
+
+Modifier une instance existante (Update)
+
+Supprimer une instance (Delete)
--- negosud_javafx/src/main/java/grp/cesi/negosud_javafx/Main.java
+++ negosud_javafx/src/main/java/grp/cesi/negosud_javafx/Main.java
@@ -4,45 +4,22 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import javafx.scene.control.Button;
-import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws IOException {
+        @Override
+        public void start(Stage stage) throws IOException {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Main.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 857, 588);
+            stage.setTitle("Negosud");
+            stage.setScene(scene);
+            stage.show();
+        }
 
-        Parent root = FXMLLoader.load(getClass().getResource("fxml/Main.fxml"));
-        primaryStage.setTitle("NegoSud");
-        //primaryStage.initStyle(StageStyle.UNDECORATED);
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();;
-        // Create a button and a label
-        //Button clickButton = new Button("Click Me!");
-        //Label outputLabel = new Label("Hello, JavaFX!");
-
-        // Set up event handling for the button
-        //clickButton.setOnAction(e -> outputLabel.setText("Button Clicked!"));
-
-        // Create a layout and add the button and label
-        //StackPane root = new StackPane();
-        //root.getChildren().addAll(clickButton, outputLabel);
-
-        // Create the scene and set it on the stage
-        //Scene scene = new Scene(root, 300, 200);
-        //primaryStage.setScene(scene);
-
-        // Set the title of the stage and show it
-        //primaryStage.setTitle("JavaFX App");
-        //primaryStage.show();
-    }
-    public static void main(String[] args) {
-        launch(args);
-    }
+        public static void main(String[] args) {
+            launch();
+        }
 }
-
